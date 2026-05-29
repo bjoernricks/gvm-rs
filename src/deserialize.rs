752feff2af05b404f8c19a3a8bef91c98c8b9202
Björn Ricks <bjoern.ricks@greenbone.net>
2026-05-29 13:29:15 +0200

    Add a deserialize function to unwrap permissions
    Allow to reduce permissions.permission[Permission{name}] to
    permissions[Permission{name}].

--- src/deserialize.rs
+++ src/deserialize.rs
@@ -4,10 +4,19 @@
 
 use serde::{Deserialize, Deserializer};
 
+use crate::commands::entity::{Permission, Permissions};
+
 pub fn unwrap_csv_string<'de, D>(deserializer: D) -> Result<Vec<String>, D::Error>
 where
     D: Deserializer<'de>,
 {
     let s = String::deserialize(deserializer)?;
     Ok(s.split(',').map(|s| s.trim().to_string()).collect())
+}
+
+pub fn unwrap_permissions<'de, D>(deserializer: D) -> Result<Vec<Permission>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    Ok(Permissions::deserialize(deserializer)?.permission)
 }
