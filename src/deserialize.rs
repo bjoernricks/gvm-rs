ac1f132107f7817cbeff928165549c41faf6b793
Björn Ricks <bjoern.ricks@greenbone.net>
2026-05-29 14:22:55 +0200

    Add a function to unwrap optional csv values from a string field

--- src/deserialize.rs
+++ src/deserialize.rs
@@ -14,6 +14,17 @@ where
     Ok(s.split(',').map(|s| s.trim().to_string()).collect())
 }
 
+pub fn unwrap_optional_csv_string<'de, D>(deserializer: D) -> Result<Option<Vec<String>>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = Option::<String>::deserialize(deserializer)?;
+    match s {
+        Some(s) => Ok(Some(s.split(',').map(|s| s.trim().to_string()).collect())),
+        None => Ok(None),
+    }
+}
+
 pub fn unwrap_permissions<'de, D>(deserializer: D) -> Result<Vec<Permission>, D::Error>
 where
     D: Deserializer<'de>,
