0da55a751c48b44bf0136d8bf42afa142535f920
Björn Ricks <bjoern.ricks@greenbone.net>
2026-08-06 12:25:55 +0200

    Add a GmpAsyncClient using tokio
    Support async by implementing a GmpAsycClient using the tokio crate.
    Currently the async implementation is behind the feature flag
    `async-tokio`.
    
    Also the async client is just a draft. It has is not yet optimized for
    concurrent GMP requests because the same connection is used. If we want
    concurrent requests we need to have dedicated connections. At the moment
    two or more instances of an async client need to be used to allow for
    concurrent requests.

---
+++ src/cli/sync_mode.rs
@@ -0,0 +1,29 @@
+// SPDX-FileCopyrightText: 2026 Björn Ricks <bjoern.ricks@gmail.com>
+//
+// SPDX-License-Identifier: GPL-3.0-or-later
+
+use gvm_rs::{
+    client::GmpClient,
+    commands::{
+        authenticate::{AuthenticateRequest, AuthenticateResponse},
+        target::{GetTargetsRequest, GetTargetsResponse},
+        version::{GetVersionRequest, GetVersionResponse},
+    },
+};
+
+pub fn run() {
+    let mut client = GmpClient::from_unix_socket_path("/tmp/gvm/gvmd/gvmd.sock").unwrap();
+
+    let version_response: GetVersionResponse = client.send_command(&GetVersionRequest).unwrap();
+    println!("Received response: {:?}", version_response);
+
+    let auth_response: AuthenticateResponse = client
+        .send_command(&AuthenticateRequest::new("admin", "admin").with_token())
+        .unwrap();
+    println!("Authentication response: {:?}", auth_response);
+
+    let targets_response: GetTargetsResponse = client
+        .send_command(&GetTargetsRequest::new().with_tasks())
+        .unwrap();
+    println!("Targets response: {:?}", targets_response);
+}
